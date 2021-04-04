47e5f75fa95ac42b464b94b817f5d27597c2a190
Vinicius G Cardozo <vinicius.gcardozo@gmail.com>
2021-04-04 02:15:52 -0300

    feat: adding theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import Header from './components/header';
 import SavingGoal from './components/saving-goal';
-import { GlobalStyle } from './styles';
+import { GlobalStyle, theme } from './styles';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <SavingGoal />
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   );
 };
 
--- src/styles.ts
+++ src/styles.ts
@@ -5,9 +5,26 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: Work Sans;
   }
 
   body {
     background-color: #F4F8FA;
   }
 `;
+
+export const theme = {
+  color: {
+    primary: '#1B31A8',
+    secondary: '#0079FF'
+  },
+  text: {
+    $blueGray900: 'rgba(30, 42, 50, 1)',
+    $blueGray600: 'rgba(77, 100, 117, 1)',
+    $blueGray400: 'rgba(112, 135, 151, 1)',
+    $blueGray300: 'rgba(138, 156, 169, 1)',
+    $blueGray100: 'rgba(203, 213, 220, 1)',
+    $blueGray50: 'rgba(233, 238, 242, 1)',
+    $blueGray10: 'rgba(244, 248, 250, 1)'
+  }
+};
