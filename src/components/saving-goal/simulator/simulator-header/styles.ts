157c974470dc742edf881fa031490679a9d84eb4
Vinicius G Cardozo <vinicius.gcardozo@gmail.com>
2021-04-04 10:53:52 -0300

    ui: centering button

--- src/components/saving-goal/simulator/simulator-header/styles.ts
+++ src/components/saving-goal/simulator/simulator-header/styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: row;
+  width: 100%;
 `;
 
 export const TextWrapper = styled.div`
--- src/components/saving-goal/simulator/styles.ts
+++ src/components/saving-goal/simulator/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background: white;
   box-shadow: 0px 16px 32px rgba(30, 42, 50, 0.08);
   border-radius: 8px;
